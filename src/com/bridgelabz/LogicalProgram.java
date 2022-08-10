ed2cce74e15894ed16eae09c2f7f17f34fa1d3f3
Apex4598 <login2ajit@gmail.com>
2022-08-10 06:24:52 +0530

    Added : Fibonacci Series Function

--- src/com/bridgelabz/LogicalProgram.java
+++ src/com/bridgelabz/LogicalProgram.java
@@ -1,5 +1,7 @@
 package com.bridgelabz;
 
+import java.util.Scanner;
+
 public class LogicalProgram {
 
     public void couponNumber(){
@@ -13,15 +15,36 @@ public class LogicalProgram {
         while (random > 0) {
             stringbuffer = stringbuffer + (chars[random % chars.length]);
             random /= chars.length;
-            System.out.println(random + "  " + random % chars.length + " string Buffer  is " + stringbuffer);
+            System.out.println(random + "  " + random % chars.length + " String Buffer  is " + stringbuffer);
         }
 
         String couponCode = stringbuffer.toString();
         System.out.println("Coupon Code: " + couponCode);
     }
+    public void fiboncciSeries(){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int num = scanner.nextInt();
+        int n1 = 0, n2 = 1, n3 = 0;
+        System.out.print(n1 + "\n" + n2+"\n");
+
+        for (int i = 2; i < num; i++)
+        {
+            n3 = n1 + n2;
+            System.out.println("" + n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Logical Program");
         LogicalProgram logicalProgram = new LogicalProgram();
+        System.out.println("Coupon Number Program");
         logicalProgram.couponNumber();
+        System.out.println("Fibonacci Series  Program");
+        logicalProgram.fiboncciSeries();
+
     }
 }
